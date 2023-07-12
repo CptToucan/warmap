69ad2497cbd80a1c4d6349525bbe500ed6a2c648
matthew.betts.96@gmail.com <matthew.betts.96@gmail.com>
2023-07-12 20:39:12 +0100

    Add shitty invert test

--- src/components/war-map.ts
+++ src/components/war-map.ts
@@ -53,16 +53,53 @@ export class WarMap extends LitElement {
     canvas.on('mouse:down', this.handleMouseDown);
   }
   render(): TemplateResult {
-    return html` <canvas
-      id="canvas"
-      styles="height: 100vh; width: 100vw"
-    ></canvas>`;
+    return html`<div>
+      <button @click="${this.handleClick}">Click me</button
+      ><canvas id="canvas" styles="height: 100vh; width: 100vw"></canvas>
+      <div></div>
+    </div>`;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   handleMouseDown(options: any) {
     const clickedObject = options.target;
     console.log(clickedObject);
+  }
+
+  handleClick() {
+    console.log('click');
+    const ctx = this.canvas.getContext('2d');
+    if (!ctx) {
+      return;
+    }
+
+    // Get the image data from the canvas
+    const imageData = ctx.getImageData(
+      0,
+      0,
+      this.canvas.width,
+      this.canvas.height
+    );
+    const data = imageData.data;
+
+    // Modify the pixel colors
+    for (let i = 0; i < data.length; i += 4) {
+      // Access the individual color channels (red, green, blue, alpha)
+      const red = data[i];
+      const green = data[i + 1];
+      const blue = data[i + 2];
+      // Modify the color values (e.g., invert the colors)
+      const invertedRed = 255 - red;
+      const invertedGreen = 255 - green;
+      const invertedBlue = 255 - blue;
+      // Assign the modified color values back to the pixel
+      data[i] = invertedRed;
+      data[i + 1] = invertedGreen;
+      data[i + 2] = invertedBlue;
+    }
+
+    // Put the modified image data back to the canvas
+    ctx.putImageData(imageData, 0, 0);
   }
 }
 
