af552f726b41242dcf3131ab7b5ee92981148e10
Matthew-Betts <matthew.betts@smartwyre.com>
2023-08-19 22:18:55 +0100

    making crows have jetpacks

--- src/algorithms/crow/crow.ts
+++ src/algorithms/crow/crow.ts
@@ -1,4 +1,4 @@
-import {findClosestColour} from './findClosestColour';
+import {findPixelDistanceSquared} from './findPixelDistance';
 
 export interface PixelData {
   x: number;
@@ -9,12 +9,43 @@ export interface PixelData {
   a: number;
 }
 
+// Note to me and other future devs, we square here as to avoid having to
+// do a computationally expensive Math.sqrt in findPixelDistance(Squared)
+
 export const crow = (
   roadPixels: PixelData[],
   redPixels: PixelData[],
   bluePixels: PixelData[]
 ) => {
-  return roadPixels.map((pixel: PixelData) =>
-    findClosestColour(pixel, redPixels, bluePixels)
-  );
+  // Precompute the closest color for each roadPixel
+  const closestColors = roadPixels.map((pixel: PixelData) => {
+    let closestColor = 'red'; // Assume red is the closest color
+    let squaredMinDistance = findPixelDistanceSquared(pixel, redPixels[0]);
+
+    for (const redPixel of redPixels) {
+      const squaredDistance = findPixelDistanceSquared(pixel, redPixel);
+      if (squaredDistance < squaredMinDistance) {
+        squaredMinDistance = squaredDistance;
+        closestColor = 'red';
+      }
+    }
+
+    for (const bluePixel of bluePixels) {
+      const squaredDistance = findPixelDistanceSquared(pixel, bluePixel);
+      if (squaredDistance < squaredMinDistance) {
+        squaredMinDistance = squaredDistance;
+        closestColor = 'blue';
+      }
+    }
+
+    return closestColor;
+  });
+
+  // Map the closest colors to the corresponding roadPixels
+  return roadPixels.map((pixel: PixelData, index: number) => {
+    const closestColor = closestColors[index];
+    return closestColor === 'red'
+      ? {...redPixels[0], x: pixel.x, y: pixel.y}
+      : {...bluePixels[0], x: pixel.x, y: pixel.y};
+  });
 };
